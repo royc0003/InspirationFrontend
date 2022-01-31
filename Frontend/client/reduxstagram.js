875b97ce15e53a45d4be300be04e95741cf274dc
Royce <royceang96@gmail.com>
2022-01-31 16:12:22 +0800

    First commit

---
+++ Frontend/client/reduxstagram.js
@@ -0,0 +1,17 @@
+// let's go!
+import React from 'react';
+
+import { render } from 'react-dom';
+
+// Import CSS
+import css from './styles/style.styl';
+
+// Import Components
+import Main from './components/Main';
+import Single from './components/Single';
+import PhotoGrid from './components/PhotoGrid';
+
+// 
+
+render(<Main/>, document.getElementById('root'));
+
