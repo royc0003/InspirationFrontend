f2e51771be8b8e7af7aff5f4fcd6ffe1a8a4f8b2
Royce <royceang96@gmail.com>
2022-02-01 17:19:09 +0800

    added comments reducer

--- Frontend/client/reducers/comments.js
+++ Frontend/client/reducers/comments.js
@@ -3,8 +3,33 @@
 // 1. the action (info about what happened)
 // 2. copy of current state
 
+function postcomments(state = [], action) {
+    switch(action.type){
+        case 'ADD_COMMENT':
+            /// return the new state with the new comment
+            return [...state, {
+                user: action.author,
+                text: action.comment
+            }];
+        
+        case 'REMOVE_COMMENT':
+            return state;
+
+        default:
+            return state;
+    }
+    return state;
+}
+
 function comments(state = [], action){
-    console.log(state, action);
+    if(typeof action.postId !== 'undefined'){
+        return {
+            // take current state
+            ...state,
+            // overwrite this post with a new one
+            [action.postId]: postcomments(state[action.postId],action)
+        }
+    }
     return state;
 }
 
