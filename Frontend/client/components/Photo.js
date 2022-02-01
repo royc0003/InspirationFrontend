6bc08a3b3362c877dc99b555919a86df6fc5bbbd
Royce <royceang96@gmail.com>
2022-02-01 14:55:07 +0800

    fixed photogrid

--- Frontend/client/components/Photo.js
+++ Frontend/client/components/Photo.js
@@ -1,11 +1,19 @@
 import React from 'React';
+import { Link } from 'react-router';
 
 const Photo = React.createClass({
     render() {
+        const { post, comments, i } = this.props;
         return (
             <figure className="grid-figure">
-                I am a photo
+                <div className="grid-photo-wrap">
+                    <Link to={`/view/${post.code}`}>
+                        {post.caption}
+                    </Link>
+                </div>
             </figure>
         )
     }
 })
+
+export default Photo;
