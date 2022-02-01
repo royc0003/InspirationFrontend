d930d3922317d4c900dec3e0b1405359b1aa9139
Royce <royceang96@gmail.com>
2022-02-01 15:59:22 +0800

    Update comments

--- Frontend/client/components/Single.js
+++ Frontend/client/components/Single.js
@@ -1,11 +1,19 @@
 import React from 'react';
-import { Link } from 'react-router';
+import Photo from './Photo';
+import Comments from './Comments';
 
 const Single = React.createClass({
   render() {
+    const { postId } = this.props.params;
+    const i = this.props.posts.findIndex((post) => post.code === postId);
+    const post = this.props.posts[i];
+
+    const postComments = this.props.comments[postId] || [];
+    console.log(post);
     return (
       <div className="single-photo">
-          I'm single.
+          <Photo i={i} post={post} {...this.props}></Photo>
+          <Comments postComments={postComments} {...this.props}/>
       </div>
     );
   },
