18dc77f2cb988985f18a89b55e68876a162247e2
Royce <royceang96@gmail.com>
2022-02-01 17:46:48 +0800

    some changes

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -8,7 +8,7 @@ module.exports = {
     './client/Index'
   ],
   output: {
-    path: path.join(__dirname, 'dist'),
+    path: path.join(__dirname, 'build'),
     filename: 'bundle.js',
     publicPath: '/static/'
   },
