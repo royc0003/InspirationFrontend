16070bb5ca8eecf9b168ca69394c9254f8699f47
Royce <royceang96@gmail.com>
2022-02-02 12:26:35 +0800

    changes to font

--- client/components/Main.js
+++ client/components/Main.js
@@ -6,7 +6,7 @@ const Main = React.createClass({
     return (
       <div>
         <h1>
-          <Link to="/">Friendstagram</Link>
+          <Link to="/"><span className="friends-start">F<span className="friends-red">.</span>R<span className="friends-blue">.</span>I<span className="friends-yellow">.</span>E<span className="friends-red">.</span>N<span className="friends-yellow">.</span>D<span className="friends-blue">.</span>S</span><span className="friends-end">tagram</span></Link>
         </h1>
         {/* Passes down props from main to the first child */}
         {React.cloneElement(this.props.children, this.props)};
