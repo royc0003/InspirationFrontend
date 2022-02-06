30f6ce14592f8ef05681bcb516def9052734631e
Royce <royceang96@gmail.com>
2022-02-06 18:58:14 +0800

    Add NavBar

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,28 +1,47 @@
-
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 
 // Import Bootstrap
-import { Navbar, Nav, Button, Container } from 'react-bootstrap';
+import { Navbar, Nav, Container } from "react-bootstrap";
 
 //Import CSS
-import  '../sass/components/_Header.scss';
-
-import styles from  '../sass/components/_Header.module.scss';
-
+import "../sass/components/_Header.scss";
+import styles from "../sass/components/_Header.module.scss";
 
 // Header Component
-export default class Header extends React.Component{
+export default class Header extends React.Component {
   render() {
+    const signOutHandler = async () => {
+      console.log('Signing')
+    }
     return (
-      <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+      <Navbar collapseOnSelect sticky="top" expand="xl" bg="dark" variant="dark">
         <Container fluid>
-        <Link to="/">
-          <Navbar.Brand className={styles.friends}><span className="friends-start">F<span className="friends-red">.</span>R<span className="friends-blue">.</span>I<span className="friends-yellow">.</span>E<span className="friends-red">.</span>N<span className="friends-yellow">.</span>D<span className="friends-blue">.</span>S</span><span className="friends-end">tagram</span></Navbar.Brand>
-        </Link>
-
+          <Link to="/">
+            <Navbar.Brand className={styles.friends}>
+              <span className="friends-start">
+                F<span className="friends-red">.</span>R
+                <span className="friends-blue">.</span>I
+                <span className="friends-yellow">.</span>E
+                <span className="friends-red">.</span>N
+                <span className="friends-yellow">.</span>D
+                <span className="friends-blue">.</span>S
+              </span>
+              <span className="friends-end">tagram</span>
+            </Navbar.Brand>
+          </Link>
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+          <Navbar.Collapse id="responsive-navbar-nav">
+            <Nav >
+              <Link to="/login">
+                <Nav.Link>Login</Nav.Link>
+              </Link>
+              <Link to="/signup">
+                <Nav.Link>Signup</Nav.Link>
+              </Link>
+            </Nav>
+          </Navbar.Collapse>
         </Container>
-        
       </Navbar>
 
       // <div>
@@ -32,7 +51,6 @@ export default class Header extends React.Component{
       //   {/* Passes down props from main to the first child */}
       //   {/* {React.cloneElement(this.props.children, this.props)}; */}
       // </div>
-    )
+    );
   }
 }
-
