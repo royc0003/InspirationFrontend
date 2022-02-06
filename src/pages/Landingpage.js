8bb17c80029276cd200027116765974dcebc8adc
Royce <royceang96@gmail.com>
2022-02-06 21:23:24 +0800

    Changes

--- src/pages/Landingpage.js
+++ src/pages/Landingpage.js
@@ -1,31 +1,29 @@
 import React from "react";
-import { Container, Button, Row, Col } from "react-bootstrap";
+import {  Button } from "react-bootstrap";
+
+// Import CSS
+import "../sass/pages/_Landingpage.scss";
 
 export function Landingpage(props) {
   return (
-    <Container>
-      <Row>
-        <div className="landingoverall">
-          <div className="Caption Title">
-            <Col xs={12} md={12} lg={12}>
-              <h1>
-                <span className="captionFirst">
-                  Boyfriends and Girlfriends Will Come And Go,
-                </span>
-              </h1>
-            </Col>
-            <Col xs={12} md={12} lg={12}>
-              <span className="captionSecond">But Friends Are For Life.</span>
-            </Col>
-          </div>
+    <div className="overallLanding">
+      <div className="overall">
+        <div className="captiontitlefirst" xs={12} md={12} lg={12}>
+          <h3>
+            <span className="captionFirst">
+              Boyfriends and Girlfriends Will Come And Go,
+            </span>
+          </h3>
         </div>
-      </Row>
-      <div className="backround-image"></div>
-      <Row>
+        <div className="captiontitlesecond" xs={12} md={12} lg={12}>
+          <h1>
+            <span className="captionSecond">But Friends Are For Life.</span>
+          </h1>
+        </div>
         <div>
-          <Button>Find Friends!</Button>
+          <Button className="landingpage-button">Find Friends!</Button>
         </div>
-      </Row>
-    </Container>
+      </div>
+    </div>
   );
 }
