69426a2f9d8b28969793292efb8af5b6c5a682e6
Royce <royceang96@gmail.com>
2022-02-06 22:02:06 +0800

    changes to colors

--- src/pages/Landingpage.js
+++ src/pages/Landingpage.js
@@ -10,17 +10,17 @@ export function Landingpage(props) {
       <div className="overall">
         <div className="captiontitlefirst" xs={12} md={12} lg={12}>
           <h3>
-            <span className="captionFirst">
+            <span className="captionFirst" style={{fontSize: "20px"}}>
               Boyfriends and Girlfriends Will Come And Go,
             </span>
           </h3>
         </div>
         <div className="captiontitlesecond" xs={12} md={12} lg={12}>
           <h1>
-            <span className="captionSecond">But Friends Are For Life.</span>
+            <span className="captionSecond" style={{fontSize: "40px", fontWeight: "bold"}}>But Friends Are For Life.</span>
           </h1>
         </div>
-        <div>
+        <div className="landingpage-button-overall">
           <Button className="landingpage-button">Find Friends!</Button>
         </div>
       </div>
