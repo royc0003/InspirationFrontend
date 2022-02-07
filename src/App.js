28bb9451cf0cfc896757814e291660f209ba71fd
Royce <royceang96@gmail.com>
2022-02-07 19:00:06 +0800

    Added protected route

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 // Redux-Store Related Imports
 import { bindActionCreators } from 'redux';
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, Navigate } from 'react-router-dom';
 import { connect } from 'react-redux';
 import * as actionCreators from './actions/actionCreators';
 
@@ -16,6 +16,7 @@ import { Login } from './pages/Login';
 import { Signup } from './pages/Signup';
 import { Landingpage } from './pages/Landingpage';
 import Formpage from './pages/Formpage';
+import auth from './reducers/auth';
 
 
 
@@ -28,7 +29,18 @@ export function App() {
         <Route path="/view/:postId" element={<Single/>}/>
         <Route path="/login" element={<Login/>}/>
         <Route path="/signup" element={<Signup/>}/>
-        <Route path="/photogrid" element={<PhotoGrid/>}/>
+        <Route
+        path="/photogrid"
+        element={
+          // Good! Do your composition here instead of wrapping <Route>.
+          // This is really just inverting the wrapping, but it's a lot
+          // more clear which components expect which props.
+          <RequireAuth redirectTo="/login">
+            <PhotoGrid/>
+          </RequireAuth>
+        }
+      />
+        {/* <Route path="/photogrid" element={<PhotoGrid/>}/> */}
         <Route path="/formpage" element={<Formpage/>}/>
       </Routes>
     </div>
@@ -40,12 +52,25 @@ function mapStateToProps(state) {
   return {
     posts: state.posts,
     comments: state.comments,
+    auth: state.auth,
   };
 }
 
 function mapDispatchToProps(dispatch) {
   return bindActionCreators(actionCreators, dispatch);
 }
+
+// https://stackoverflow.com/questions/62384395/protected-route-with-react-router-v6
+function RequireAuth({ children, redirectTo }) {
+  if(auth.isLoading) {
+    return <h2>Loading...</h2>
+  }
+  else if(!auth.isAuthenticated){
+    return <Navigate to="/login"/>
+  }else {
+    <Navigate to={redirectTo}  />
+  }
+};
 
 export default connect(mapStateToProps, mapDispatchToProps)(App);
 
