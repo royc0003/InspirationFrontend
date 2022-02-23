01e436dc197f807b9c5635cf1de5694ccc6c34d1
Royce <royceang96@gmail.com>
2022-02-23 23:52:36 +0800

    fixed indentation

--- src/components/Toasts.js
+++ src/components/Toasts.js
@@ -2,17 +2,22 @@ import React, { useState } from "react";
 import { Toast } from "react-bootstrap";
 
 export function Toasts(props) {
-  const { item } = props; 
+  const { item } = props;
   const [show, setShow] = useState(true);
   return (
-    <Toast onClose={() => setShow(false)} show={show} delay={8000} autohide className="d-inline-block m-1" bg={"warning"}>
-    <Toast.Header>
-      <img src="holder.js/20x20?text=%20" className="rounded me-2" alt="" />
-      <strong className="me-auto">Error</strong>
-    </Toast.Header>
-    <Toast.Body >
-        {item}
-    </Toast.Body>
-  </Toast> 
+    <Toast
+      onClose={() => setShow(false)}
+      show={show}
+      delay={8000}
+      autohide
+      className="d-inline-block m-1"
+      bg={"warning"}
+    >
+      <Toast.Header>
+        <img src="holder.js/20x20?text=%20" className="rounded me-2" alt="" />
+        <strong className="me-auto">Error</strong>
+      </Toast.Header>
+      <Toast.Body>{item}</Toast.Body>
+    </Toast>
   );
 }
