452156a019e59ce5b2d7e10dd7a83df96157eb1a
Royce <royceang96@gmail.com>
2022-02-26 20:51:16 +0800

    Included Row And Col

--- src/pages/Questionaire3.js
+++ src/pages/Questionaire3.js
@@ -1,14 +1,24 @@
 import React, { useState } from "react";
+
+// Bootstrap related componentes
 import RangeSlider from "react-bootstrap-range-slider";
+import { Container, Row, Col } from "react-bootstrap";
 
 export function Questionaire3(props) {
   const [value, setValue] = useState(0);
   return (
     <div className="qn3-overall">
-      <RangeSlider
-        value={value}
-        onChange={(changeEvent) => setValue(changeEvent.target.value)}
-      />
+      <Container>
+        <Row>
+          <Col sm={12} md={4} lg={2}>Something</Col>
+          <Col sm={12} md={8}  lg={10}>
+            <RangeSlider
+              value={value}
+              onChange={(changeEvent) => setValue(changeEvent.target.value)}
+            />
+          </Col>
+        </Row>
+      </Container>
     </div>
   );
 }
