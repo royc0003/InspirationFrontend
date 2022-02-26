4721caaf7d74dd48f25531922a13581e04c027b1
Royce <royceang96@gmail.com>
2022-02-26 16:01:02 +0800

    Included svg and jpg

--- src/pages/Questionaire2.js
+++ src/pages/Questionaire2.js
@@ -1,7 +1,24 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Button } from "react-bootstrap";
 
+// Import Redux Related Components/Library
+import { useDispatch, useSelector } from "react-redux";
+import { getinterests } from '../actions/question2';
+
 export function Questionaire2(props) {
+  // Set All States
+  // const interests = useSelector(state => state.question2.interests);
+
+  // Set Dispatch
+  const dispatch = useDispatch();
+
+  // componentDidMount() -> Retrieve all interests.
+  useEffect(() => {
+    // dispatch(getinterests())
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
+
+  // Make sure to do error handling.
     // destructuring
   const { nextQuestionHandler } = props;
   return (
