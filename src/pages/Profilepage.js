8a6e4621ffde6eedb6cd8bf60e5932492a0eeaec
thz <huizhantan@gmail.com>
2022-03-18 16:06:14 +0800

    fix bad css in profile page and app component

--- src/pages/Profilepage.js
+++ src/pages/Profilepage.js
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { Button, Card, CardGroup, Col, Container, Figure, Image, Row } from "react-bootstrap";
+import { Button, Card, Col, Container, Figure, Image, Row } from "react-bootstrap";
 import { useDispatch, useSelector } from "react-redux";
 import { ClimbingBoxLoader } from "react-spinners";
 import {
@@ -24,6 +24,8 @@ export function Profilepage(props) {
 	const isComplete = useSelector((state) => state.profilepage.isComplete);
 	const { user_info, flatten_matched_users, all_interests, flatten_user_interests } = useSelector((state) => state.profilepage)
 
+	const debug = false;
+
 	// Similar to React's Component Did Mount
 	useEffect(() => {
 		const retrieveAllDataPromise = async () => {
@@ -47,105 +49,104 @@ export function Profilepage(props) {
 	}, [flatten_matched_users]);
 
 	return (
-		<div>
-			{!isComplete ? (
-				<Container className="spinner-class">
-					<Row className="justify-content-xs-center">
-						<Col
-							xs={{ span: 6, offset: 5 }}
-							md={{ span: 6, offset: 6 }}
-							lg={{ span: 6, offset: 6 }}
-							xl={{ span: 6, offset: 6 }}
-						>
-							<ClimbingBoxLoader color={color} loading={loading} size={20} />
-						</Col>
-					</Row>
-				</Container>
-			) :
-				(
-					<Container>
-						<Row className={styles.rowSection}>
-							<h1>My Profile</h1>
-						</Row>
-						<Row className="d-flex justify-content-center">
-							<Image
-								id={styles.profileImage}
-								src={user_info.pic_url}></Image>
-
-						</Row>
+		<Container id={styles.profilePageContainer}>
+			{
+				!isComplete
+					?
+					(
 						<Row>
-							<div className="d-flex justify-content-center mt-5">
-								<div className="d-flex flex-column">
-									<div className={styles.profileInfo}>
-										<span className="mark"><strong>Email:</strong></span>
-										<div className="sub-comments">{user_info.email}</div>
-									</div>
-									<div className={styles.profileInfo}>
-										<span className="mark"><strong>Bio:</strong></span>
-										<div className="sub-comments">{user_info.biography ? user_info.biography : "User does not have a bio"}</div>
-									</div>
-									<div className={styles.profileInfo}>
-										<span className="mark"><strong>Hall:</strong></span>
-										<div className="sub-comments">
-											{user_info.hall ? user_info.hall : "User does not have hall"}
+							<div id={styles.loader}>
+								<ClimbingBoxLoader color={color} loading={loading} size={20} />
+							</div>
+						</Row>
+					)
+					:
+					(
+						<div>
+							<Row className={styles.rowSection}>
+								<h1>My Profile</h1>
+							</Row>
+							<Row className="d-flex justify-content-center">
+								<Image
+									id={styles.profileImage}
+									src={user_info.pic_url}></Image>
+
+							</Row>
+							<Row>
+								<div className="d-flex justify-content-center mt-5">
+									<div className="d-flex flex-column">
+										<div className={styles.profileInfo}>
+											<span className="mark"><strong>Email:</strong></span>
+											<div className="sub-comments">{user_info.email}</div>
+										</div>
+										<div className={styles.profileInfo}>
+											<span className="mark"><strong>Bio:</strong></span>
+											<div className="sub-comments">{user_info.biography ? user_info.biography : "User does not have a bio"}</div>
+										</div>
+										<div className={styles.profileInfo}>
+											<span className="mark"><strong>Hall:</strong></span>
+											<div className="sub-comments">
+												{user_info.hall ? user_info.hall : "User does not have hall"}
+											</div>
 										</div>
 									</div>
 								</div>
-							</div>
-						</Row>
+							</Row>
 
-						{/* Selected Interests */}
-						<Row className={styles.rowSection}>
-							<h2>Selected Interests</h2>
-							{
-								mock_interests.map((v, i) => {
-									return (
-										<Col xs={2} key={v.id}>
-											<Figure>
-												<Figure.Image
-													width={100}
-													height={100}
-													alt="200x200"
-													src="https://picsum.photos/100/"
-												/>
-												<Figure.Caption className={styles.figureCaption}>
-													{v.interest.charAt(0).toUpperCase() + v.interest.slice(1)}
-												</Figure.Caption>
-											</Figure>
-										</Col>
-									);
-								})
-							}
-						</Row>
-
-						{/* Matched History */}
-						<Row className={styles.rowSection}>
-							<h2>Matched History</h2>
-							<CardGroup>
+							{/* Selected Interests */}
+							<Row className={styles.rowSection}>
+								<h2>Selected Interests</h2>
 								{
-									flatten_matched_users.map((matched_user) => {
+									mock_interests.map((v, i) => {
 										return (
-											<Col xs={4} key={matched_user.id}>
-												<Card >
-													<Card.Img variant="top" src={matched_user.pic_url} />
-													<Card.Body>
-														<Card.Title>{matched_user.email.substr(0, matched_user.email.indexOf('@'))}</Card.Title>
-														<Card.Text>
-															<span style={{fontStyle: 'italic'}}>
-																{matched_user.biography ?? "This user does not have a biography!" }
-															</span>
-														</Card.Text>
-														<Button variant="secondary">View Profile</Button>
-													</Card.Body>
-												</Card>
+											<Col xs={2} key={v.id}>
+												<Figure>
+													<Figure.Image
+														width={100}
+														height={100}
+														alt="200x200"
+														src="https://picsum.photos/100/"
+													/>
+													<Figure.Caption className={styles.figureCaption}>
+														{v.interest.charAt(0).toUpperCase() + v.interest.slice(1)}
+													</Figure.Caption>
+												</Figure>
 											</Col>
-										)
+										);
 									})
 								}
-							</CardGroup>
-						</Row>
-					</Container>
-				)}
-		</div>
+							</Row>
+
+							{/* Matched History */}
+							<Row className={styles.rowSection}>
+								<h2>Matched History</h2>
+								<Row className="g-4" xs={3}>
+									{
+										flatten_matched_users.map((matched_user) => {
+											return (
+												<Col xs={3} key={matched_user.id}>
+													<Card className={styles.matchedUserCard}>
+														<Card.Img variant="top" src={matched_user.pic_url} />
+														<Card.Body>
+															<Card.Title>{matched_user.email.substr(0, matched_user.email.indexOf('@'))}</Card.Title>
+															<Card.Text>
+																<span style={{ fontStyle: 'italic' }}>
+																	{matched_user.biography ?? "This user does not have a biography!"}
+																</span>
+															</Card.Text>
+														</Card.Body>
+														<Card.Footer>
+															<Button variant="secondary">View Profile</Button>
+														</Card.Footer>
+													</Card>
+												</Col>
+											)
+										})
+									}
+								</Row>
+							</Row>
+						</div>
+					)}
+		</Container>
 	);
 }
